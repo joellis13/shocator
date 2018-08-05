ec74b46500f5768c4575db3a7ecdbf61ec706ff7
Jonathan Ellis <joellis13@yahoo.com>
2018-08-04 22:21:18 -0500

    finished rough search results process (search confirmation)

--- src/main/java/com/example/shocator/controllers/SearchController.java
+++ src/main/java/com/example/shocator/controllers/SearchController.java
@@ -4,13 +4,29 @@ package com.example.shocator.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SearchController {
 
-    @RequestMapping(value = "")
+    static String title = "Shocator";
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("title", "Shocator");
+        model.addAttribute("title", title);
         return "index";
     }
+
+    @RequestMapping(value = "search", method = RequestMethod.POST)
+    public String searchResults(@RequestParam String name, Model model) {
+
+        String message = "You searched for '" + name + ".'";
+
+        model.addAttribute("message", message);
+        model.addAttribute("title", title);
+
+        return "search";
+    }
+
 }
